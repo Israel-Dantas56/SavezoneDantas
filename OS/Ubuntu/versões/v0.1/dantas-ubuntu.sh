738ce1eb31f7aed7f3a7e9c2b09117f861e5a20e
Israel-Dantas56 <israeldantas56@hotmail.com>
2025-04-02 01:59:38 -0300

    criação das pastas do repositório

---
+++ OS/Ubuntu/versões/v0.1/dantas-ubuntu.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#################################################################
+# sistema - Script para Instalação de programas do Ubuntu       #
+# Autor: ISRAEL DANTAS                                          #
+# Data: 02/04/2025                                              #
+# Atualizado: 02/04/2025  autor: ISRAEL DANATS                  #
+#################################################################
+
+clear
+
+
